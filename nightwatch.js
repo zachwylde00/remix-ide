cbf92a7c1920b9e27aa0f6f66d4ca7e9f76bb97e
ioedeveloper <daviddisu8@gmail.com>
2020-02-18 14:36:10 +0000

    Debug pipeline

--- nightwatch.js
+++ nightwatch.js
@@ -60,7 +60,7 @@ module.exports = {
         'browserName': 'firefox',
         'javascriptEnabled': true,
         'acceptSslCerts': true,
-        'marionette': start
+        'marionette': true
       }
     }
   }
